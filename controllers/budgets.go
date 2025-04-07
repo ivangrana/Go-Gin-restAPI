beb6651b54fbf04f720d39ffb9d3a324e48ef2e3
ivangrana <ivangrana1956@gmail.com>
2025-04-07 11:05:39 -0300

    Budgetservice integrated to controller;

--- controllers/budgets.go
+++ controllers/budgets.go
@@ -1,40 +1,22 @@
 package controllers
 
 import (
-	"Phinance/database"
-	DTOs "Phinance/dto"
-	"Phinance/models"
+	dto "Phinance/dto"
+	"Phinance/services"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
-func GetAllBudgets(c *gin.Context) {
-	var budgets []models.Budget
-	var budgetDTOs []DTOs.BudgetDTO
+var budgetService *services.BudgetService
 
-	resp := database.DB.Find(&budgets, "user_ID = ?", c.Param("id"))
-	if resp.Error != nil {
-		// Check if error is not due to no rows found
-		if resp.Error == gorm.ErrRecordNotFound {
-			c.JSON(http.StatusNotFound, gin.H{"error": "Budget not found"})
-		} else {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": resp.Error.Error()})
-		}
+func GetAllBudgets(c *gin.Context) {
+	budgets, err := budgetService.GetAllBudgets(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	for _, budget := range budgets {
-		budgetDTO := DTOs.BudgetDTO{
-			ID:          budget.ID,
-			LimitValue:  budget.LimitValue,
-			InitialDate: budget.InitialDate,
-			FinalDate:   budget.FinalDate,
-		}
-		budgetDTOs = append(budgetDTOs, budgetDTO)
-	}
-	c.JSON(http.StatusOK, budgetDTOs)
+	c.JSON(http.StatusOK, budgets)
 }
 
 func GetBudgetById(c *gin.Context) {
@@ -42,52 +24,47 @@ func GetBudgetById(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "budget_id is required"})
 		return
 	}
-	var budget models.Budget
-	var budgetDTO DTOs.BudgetDTO
 
-	resp := database.DB.First(&budget, c.Param("budget_id"))
-	if resp.Error != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": resp.Error.Error()})
-		return
-	}
-	budgetDTO = DTOs.BudgetDTO{
-		ID:          budget.ID,
-		LimitValue:  budget.LimitValue,
-		InitialDate: budget.InitialDate,
-		FinalDate:   budget.FinalDate,
-	}
-	c.JSON(http.StatusOK, budgetDTO)
-}
+	budget, err := budgetService.GetBudgetById(c.Param("budget_id"))
 
-func CreateBudget(c *gin.Context) {
-	var budget models.Budget
-	var budgetDTO DTOs.BudgetCreateDTO
-
-	//convert string to uint
-	id, _ := strconv.Atoi(c.Param("id"))
-
-	err := c.ShouldBindJSON(&budgetDTO)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
-	budget.UserID = uint(id)
-	budget.LimitValue = budgetDTO.LimitValue
-
-	database.DB.Create(&budget)
-
-	c.JSON(http.StatusCreated, gin.H{"message": "budget created successfully"})
+	c.JSON(http.StatusOK, budget)
 }
 
-func DeleteBudget(c *gin.Context) {
-	var budget models.Budget
-	id := c.Param("budget_id")
-
-	if err := database.DB.Delete(&budget, "id = ?", id).Error; err != nil {
+func CreateBudget(c *gin.Context) {
+	var budgetDTO dto.BudgetCreateDTO
+	if err := c.ShouldBindJSON(&budgetDTO); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"Response": "budget deleted"})
+	if c.Param("id") == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "user id is required"})
+		return
+	}
+
+	resp := budgetService.CreateBudget(c.Param("id"), budgetDTO)
+
+	if resp != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": resp.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, resp)
+}
+
+func DeleteBudget(c *gin.Context) {
+	id := c.Param("budget_id")
+
+	err := budgetService.DeleteBudget(id)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"success": "budget deleted"})
+
 }
